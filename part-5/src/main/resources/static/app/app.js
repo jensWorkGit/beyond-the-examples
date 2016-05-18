6df21d1b6863c4cff544a6697eff6ad31a8d8a6a
jens <jho@orgcomnet.de>
2016-05-18 21:51:15 +0200

    * added files to gititgnore

---
+++ part-5/src/main/resources/static/app/app.js
@@ -0,0 +1,21 @@
+angular
+    .module('myApp')
+    .controller('mainController', mainController);
+
+mainController.$inject = ['$scope'];
+
+/* @ngInject */
+function mainController($scope) {
+    /* jshint validthis: true */
+    var vm = this;
+
+    vm.activate = activate;
+    vm.title = 'mainController';
+
+    activate();
+
+    ////////////////
+
+    function activate() {
+    }
+}
